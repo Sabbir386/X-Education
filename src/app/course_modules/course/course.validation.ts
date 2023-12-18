4d2980449c9e2dfe9ec897dc330c7dc6e787d4eb
sabbir ahmed <sabbir386.sa@gmail.com>
2023-12-19 00:10:53 +0600

    added course validation schema

---
+++ src/app/course_modules/course/course.validation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const ScheduleValidationSchema = z.object({
+  startDate: z.string(),
+  endDate: z.string(),
+  classDays: z.array(z.string()),
+  classTime: z.string(),
+});
+export const CourseValidationSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  price: z.string(),
+  duration: z.string(),
+  level: z.string(),
+  topics: z.array(z.string()),
+  schedule: ScheduleValidationSchema,
+  isDeleted: z.boolean().optional(),
+});
+
+export default CourseValidationSchema;
